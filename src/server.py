0c70fab102c30a8ad9adfd262575185da42fb0e1
alejdg <alexandre.gomes@canonical.com>
2024-07-19 14:17:13 -0700

    Create run_bash function and fix route definition

--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+import subprocess
 from flask import Flask, jsonify
 import yaml
 
@@ -12,12 +13,23 @@ class CharmAssistantAPIServer:
             return yaml.safe_load(f)
 
     def configure_routes(self):
-        print(self.config)
         for action in self.config["actions"]:
-            route_path = action["name"].replace(" ", "_").to_lower()
-            route_func_name = action["func"]
-            route_func = getattr(self, route_func_name)
-            self.app.route(route_path, methods=["GET"])(route_func)
+            route_name = action["name"].replace(" ", "_").lower()
+            route_path = "/" + route_name
+
+            # Define a closure to capture the command for each route
+            def make_route_func(cmd):
+                def route_func():
+                    output = self.run_bash_command(cmd)
+                    return jsonify({"output": output})
+
+                return route_func
+
+            # Create the route function with the specific command
+            route_func = make_route_func(action["cmd"])
+
+            # Add the route to the Flask app
+            self.app.route(route_path, methods=["GET"], endpoint=route_name)(route_func)
 
     def run(self):
         self.configure_routes()
@@ -28,6 +40,19 @@ class CharmAssistantAPIServer:
 
     def unprotected_route(self):
         return jsonify({"message": "Unprotected API is accessed"})
+
+    def run_bash_command(self, command):
+        try:
+            # Execute the command
+            result = subprocess.run(
+                command, shell=True, check=True, text=True, capture_output=True
+            )
+
+            # Return the output
+            return result.stdout
+        except subprocess.CalledProcessError as e:
+            # Handle errors in the called executable
+            return f"An error occurred: {e.stderr}"
 
 
 if __name__ == "__main__":
